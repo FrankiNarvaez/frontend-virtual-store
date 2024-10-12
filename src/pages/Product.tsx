66db0f80fea5ad1cb6c53d0ec6c0894fa03dddfa
FrankiNarvaez <frannki03@gmail.com>
2024-10-12 18:49:55 -0500

    Added Page to see an product

---
+++ src/pages/Product.tsx
@@ -0,0 +1,9 @@
+import InfoProduct from "../components/InfoProduct";
+
+export default function Product() {
+  return (
+    <section className="mx-[5%] lg:mx-[15%] 2xl:mx-[25%] pt-14">
+      <InfoProduct />
+    </section>
+  )
+}

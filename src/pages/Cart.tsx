dfec7d9de8e9f1b354e575c4bd704153c1042a5c
FrankiNarvaez <frannki03@gmail.com>
2024-10-13 14:53:37 -0500

    Added funcionality to buy the products

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -26,18 +26,50 @@ export default function Cart() {
     }
   }, [])
 
+  const handleBuyProducts = async () => {
+    const productsToOrder = products?.map((product) => {
+      return {
+        product_id: product.product.id,
+        quantity: product.quantity
+      }
+    })
+    const user_id = localStorage.getItem("user_id")
+    const token = JSON.parse(localStorage.getItem("access_token") as string).access_token
+
+    await api.post(`orders/create/${user_id}`, {
+      products: productsToOrder
+    }, {
+      headers: {
+        access_token: token
+      }
+    })
+  }
+
+  let total = 0
   return (
     <section className="px-[5%] lg:px-[10%] 2xl:px-[15%] mt-5 flex flex-col gap-5">
-      {products?.map((product) => (
-        <ProductCart
-          key={product.product.id}
-          id={product.product.id}
-          name={product.product.name}
-          image={product.product.image}
-          price={product.product.price}
-          quantity={+product.quantity}
-        />
-      ))}
+      {products?.map((product) => {
+        // Acumulamos el total
+        total += +product.quantity * +product.product.price;
+        return (
+          <ProductCart
+            key={product.product.id}
+            id={product.product.id}
+            name={product.product.name}
+            image={product.product.image}
+            price={product.product.price}
+            quantity={+product.quantity}
+          />
+        );
+      })}
+      {products?.length === 0 || products === undefined ? 
+        <h3 className="text-center font-bold text-xl">You no have products in the cart, lets to add</h3>
+      : 
+        <div className="px-10 py-4 rounded-md font-semibold text-lg flex justify-between items-center">
+          Total: ${total}
+          <button onClick={handleBuyProducts} className="bg-sky-500 py-2 px-7 rounded-md">Buy Products</button>
+        </div>
+      }
     </section>
   )
 }

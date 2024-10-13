7c45baf154616921eaf13c87372593e3eb3e24f2
FrankiNarvaez <frannki03@gmail.com>
2024-10-13 02:04:12 -0500

    Added verification to see if token is not expired

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Product from "./pages/Product";
 import Search from "./pages/Search";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { useEffect } from "react";
+import { localStorageToken } from "./types/types";
 
 function App() {
+  const location = useLocation()
+  const { pathname } = location;
+
+  useEffect(() => {
+    const timeNow = new Date()
+    const timeToExpyreToken = localStorage.getItem("access_token") as localStorageToken | null
+    if (timeToExpyreToken) {
+      if (timeNow.getTime() > timeToExpyreToken?.expiry) {
+        localStorage.removeItem("access_token")
+        localStorage.setItem("isLoggedIn", "false")
+      }
+    }
+  }, [])
+
   return (
     <>
-      <Header />
+      {pathname !== '/login' && pathname !== '/register' && <Header />}
       <Routes>
         <Route index element={ <Home /> } />
         <Route path='/product/:id' element={ <Product /> } />
         <Route path="/search" element={ <Search /> } />
+        <Route path="/login" element={ <Login /> } />
+        <Route path="/register" element={ <Register /> } />
       </Routes>
     </>
   )

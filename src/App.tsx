9cfb1b08c0aaf056f40c190da2c64f3d3a2a3547
FrankiNarvaez <frannki03@gmail.com>
2024-10-12 20:59:40 -0500

    Added route to search

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Product from "./pages/Product";
+import Search from "./pages/Search";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route index element={<Home />} />
-        <Route path='/product/:id' element={<Product />} />
+        <Route index element={ <Home /> } />
+        <Route path='/product/:id' element={ <Product /> } />
+        <Route path="/search" element={ <Search /> } />
       </Routes>
     </>
   )

83757647f49a86207bce3996658b1442dd03db43
FrankiNarvaez <frannki03@gmail.com>
2024-10-13 04:42:14 -0500

    Added funcionality to add product the cart

--- src/components/InfoProduct.tsx
+++ src/components/InfoProduct.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { resultProduct } from "../types/types";
+import { localStorageToken, resultProduct } from "../types/types";
 import { api } from "../lib/api";
 import { useParams } from 'react-router-dom'
 
@@ -13,6 +13,26 @@ export default function InfoProduct() {
       setProduct(data)
     })()
   }, [id])
+
+  const handleAddToCart = async () => {
+    try {
+      const token: localStorageToken = JSON.parse(localStorage.getItem("access_token") as string)
+      const user_id = localStorage.getItem("user_id")
+
+      const { data } = await api.post(`shopping-cart/add-product/${user_id}`, {
+        product_id: id,
+        quantity: 1
+      }, {
+        headers: {
+          access_token: token?.access_token
+        },
+        
+      })
+    console.log(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
 	return (
     <section className="flex flex-col rounded-lg shadow-lg p-6 sm:gap-4 md:gap-14 bg-white sm:flex-row">
@@ -28,7 +48,7 @@ export default function InfoProduct() {
           <span className="text-xl flex">${product?.price}</span>
           <span className="text-sm bg-[#e0e0e0] px-2 py-1 rounded-md">{product?.stock} in stock</span>
         </div>
-        <button className="flex justify-center item-center py-3 rounded-md bg-sky-500 hover:bg-sky-800 font-semibold text-white" >Add to cart</button>
+        <button onClick={handleAddToCart} className="flex justify-center item-center py-3 rounded-md bg-sky-500 hover:bg-sky-800 font-semibold text-white" >Add to cart</button>
       </div>
     </section>
 	)

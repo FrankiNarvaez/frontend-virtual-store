b2c4ef281619cc17863ee7647e46720e845322c3
FrankiNarvaez <frannki03@gmail.com>
2024-10-13 13:19:32 -0500

    Added page to see orders

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from 'react'
+import { Order } from '../interfaces/orders.interface'
+import { api } from '../lib/api'
+import OrderItem from '../components/Orders';
+
 export default function Orders() {
+  const [orders, setOrders] = useState<Order[]>();
+  
+  useEffect(() => {
+    (async () => {
+      const user_id = localStorage.getItem("user_id")
+      const token = JSON.parse(localStorage.getItem("access_token") as string).access_token
+  
+      const { data } = await api.get(`orders/get-orders/${user_id}`, {
+        headers: {
+          access_token: token
+        }
+      })
+      setOrders(data)
+    })()
+  }, [])
+
   return (
-    <h4>Tengo muchas ordenes</h4>
+    <div className="container mx-auto px-4 py-8">
+      <h2 className="text-2xl font-bold mb-6">My orders</h2>
+      {orders?.map((order) => (
+        <OrderItem 
+          key={order.id} 
+          bought_at={order.bought_at} 
+          id={order.id}
+          products_includes={order.products_includes}
+          total={order.total}  
+        />
+      ))}
+    </div>
   )
 }
+

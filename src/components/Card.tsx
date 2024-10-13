20426c2810738534c6745d8f0e7bbae81d866fb9
FrankiNarvaez <frannki03@gmail.com>
2024-10-13 04:40:27 -0500

    Added funcionality to add product the cart

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,32 @@
 import { Link } from "react-router-dom";
-import { resultProduct } from "../types/types";
+import { localStorageToken, resultProduct } from "../types/types";
+import { api } from "../lib/api";
 
 export default function Card(props: resultProduct) {
+  const handleAddToCart = async () => {
+    try {
+      const token: localStorageToken = JSON.parse(localStorage.getItem("access_token") as string)
+      const user_id = localStorage.getItem("user_id")
+
+      const { data } = await api.post(`shopping-cart/add-product/${user_id}`, {
+        product_id: props.id,
+        quantity: 1
+      }, {
+        headers: {
+          access_token: token?.access_token
+        },
+        
+      })
+    console.log(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <section className="max-w-80 bg-white rounded-lg overflow-hidden shadow-md p-2 mx-auto">
       <img 
-        className="w-full h-48 object-cover"
+        className="w-60 sm:w-72 md:w-80 h-48 object-cover"
         src={props.image}
         alt={props.name}
       />
@@ -16,7 +37,7 @@ export default function Card(props: resultProduct) {
           <span className="text-sm bg-[#e0e0e0] px-2 py-1 rounded-md">{props.stock} in stock</span>
         </div>
         <div className="flex gap-4">
-          <button className="block font-semibold w-full px-0 py-2 bg-sky-500 text-white text-center text-base border-none rounded-md cursor-pointer hover:bg-sky-600">Add to cart</button>
+          <button onClick={handleAddToCart} className="block font-semibold w-full px-0 py-2 bg-sky-500 text-white text-center text-base border-none rounded-md cursor-pointer hover:bg-sky-600">Add to cart</button>
           <Link to={`/product/${props.id}`} className="block font-semibold w-full px-0 py-2 bg-sky-500 text-white text-center text-base border-none rounded-md cursor-pointer hover:bg-sky-600">Preview</Link>
         </div>
       </div>

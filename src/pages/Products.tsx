ea13e405e273a72a04c446c327fed836dfd27792
FrankiNarvaez <frannki03@gmail.com>
2024-10-14 02:31:39 -0500

    Removed backdrop-blur

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -3,10 +3,11 @@ import { Link, Outlet } from "react-router-dom"
 export default function Products() {
   return (
     <div className="flex flex-col pt-20 md:flex-row px-[5%] lg:px-[10%] 2xl:px-[15%]">
-      <aside className="md:fixed md:h-5/6 backdrop-blur-xl p-4 bg-[rgba(3,7,18,0.5)] m-3 rounded-md">
+      <aside className="md:fixed md:h-5/6 p-4 bg-[rgba(3,7,18,0.5)] m-3 rounded-md">
         <ul className="flex flex-row md:flex-col items-center justify-around md:justify-center h-full">
           <li className="text-white font-semibold hover:bg-[rgba(3,7,18,0.09)] p-2 rounded-md"><Link to="/products/create">Add products</Link></li>
           <li className="text-white font-semibold hover:bg-[rgba(3,7,18,0.09)] p-2 rounded-md"><Link to="/products/">Edit products</Link></li>
+          <li className="text-white font-semibold hover:bg-[rgba(3,7,18,0.09)] p-2 rounded-md"><Link to="/products/orders">Orders</Link></li>
         </ul>
       </aside>
 

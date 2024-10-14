49711050689c41986b759ee9bc7dd257519c4be2
FrankiNarvaez <frannki03@gmail.com>
2024-10-13 22:19:16 -0500

    Show prudcts

--- src/pages/EditProducts.tsx
+++ src/pages/EditProducts.tsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react"
+import { api } from "../lib/api"
+import { resultProduct } from "../types/types"
+import EditProduct from "../components/EditProduct"
+
 export default function EditProducts() {
+  const [products, setProducts] = useState<resultProduct[]>()
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await api.get("products/get-products")
+      setProducts(data)
+    })()
+  }, [])
+  
   return (
-    <h4>Holas</h4>
+    <section className="flex flex-col gap-6">
+      {products?.map((product) => (
+        <EditProduct
+          key={product.id}
+          id={product.id}
+          image={product.image}
+          name={product.name}
+          price={product.price}
+          description={product.description}
+          stock={product.stock}
+        />
+      ))}
+    </section>
   )
 }
